677a6452240d011ed840e2e12eb7bedf2cd0a6df
siyam-x <sisyeu01@gmail.com>
2026-09-06 12:32:38 +0600

    Create config.js

---
+++ config.js
@@ -0,0 +1,13 @@
+module.exports = {
+    ownerID: 61591371186179,
+
+    adminIDs: [
+        61591371186179
+    ],
+
+    whitelistMode: false,
+
+    whitelistedIDs: [
+        61591371186179
+    ]
+};
