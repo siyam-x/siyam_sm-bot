0e37a52eb5f70c31b877593bbf4587f98273655c
siyam-x <sisyeu01@gmail.com>
2026-09-06 12:24:17 +0600

    Create node runner.js

---
+++ node runner.js
@@ -0,0 +1,30 @@
+const { spawn } = require('child_process');
+
+process.on('uncaughtException', (err) => {
+    console.error('Crash Prevented - Uncaught Exception:', err);
+});
+
+process.on('unhandledRejection', (reason, promise) => {
+    console.error('Crash Prevented - Unhandled Rejection:', reason);
+});
+
+function startBot() {
+    console.log("Telegram Bot Starting...");
+
+    const botProcess = spawn('node', ['index.js'], {
+        stdio: 'inherit',
+        shell: true
+    });
+
+    botProcess.on('close', (code) => {
+        console.log(`Bot stopped with code ${code}. Restarting in 3 seconds...`);
+        setTimeout(startBot, 3000);
+    });
+
+    botProcess.on('error', (err) => {
+        console.error('Bot Error:', err);
+        setTimeout(startBot, 3000);
+    });
+}
+
+startBot();
