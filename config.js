cfd9b33c72eb4b2ef11645f533b4d0b511ab220d
siyam-x <sisyeu01@gmail.com>
2026-09-10 15:18:38 +0600

    Update config.js to include new properties

--- config.js
+++ config.js
@@ -1,13 +1,22 @@
 module.exports = {
+    botToken: "8973277623:AAE8rALePkquP5UaQJNShK45U8AQ-Q6VBlc",
+
+    prefix: "/",
+
     ownerID: 8442705758,
 
     adminIDs: [
         8442705758
     ],
 
-    whitelistMode: false,
+    role1IDs: [],
 
+    whitelistMode: false,
     whitelistedIDs: [
         8442705758
-    ]
+    ],
+
+    bannedUsers: [],
+
+    timeZone: "Asia/Dhaka"
 };
