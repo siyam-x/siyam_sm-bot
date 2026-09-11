0cabd2b6b109e87578e7e4dd3d012245a20b5a5c
siyam-x <sisyeu01@gmail.com>
2026-09-12 01:04:01 +0600

    Update config.js

--- config.js
+++ config.js
@@ -1,14 +1,19 @@
-module.exports = {
-    botToken: '8973277623:AAE8rALePkquP5UaQJNShK45U8AQ-Q6VBlc',
-    prefix: '/',
-    ownerID: 8442705758,
+require('dotenv').config();
 
+module.exports = {
+    botToken: process.env.BOT_TOKEN || '8973277623:AAE8rALePkquP5UaQJNShK45U8AQ-Q6VBlc',
+    prefix: ',',
+    ownerID: 8442705758,
+    
+    // Role 2: Super Admin / Owner
     adminIDs: [
         8442705758
     ],
 
-    whitelistMode: false,
+    // Role 1: Moderator / Sub-Admin
+    modIDs: [],
 
+    whitelistMode: false,
     whitelistedIDs: [
         8442705758
     ]
