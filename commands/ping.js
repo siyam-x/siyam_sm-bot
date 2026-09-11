acf4496cd667cf42ed3029b60aa37aa437d9d43c
siyam-x <sisyeu01@gmail.com>
2026-09-12 01:33:41 +0600

    Delete commands/ping.js

--- commands/ping.js
+++
@@ -1,9 +0,0 @@
-module.exports = {
-    name: "ping",
-    aliases: ["p", "test"],
-    role: 0,
-    execute: async (bot, msg, args, extra) => {
-        const chatId = msg.chat.id;
-        return bot.sendMessage(chatId, "Pong! 🏓 Bot is working perfectly.");
-    }
-};
